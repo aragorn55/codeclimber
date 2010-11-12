285bc35823cf7d5c05496f4b1005cf2125b563fc
simone.chiaretta <simone.chiaretta@65309fa4-5e35-0410-8ec5-af3afdd08594>
2010-11-12 16:15:19 +0000

    Adding some more files

---
+++ GoogleReaderConnector/trunk/ReaderConnector/ReaderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeClimber.GoogleReaderConnector
+{
+    public class ReaderService : IDisposable
+    {
+        private const string SERVICENAME = "reader";
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ClientName { get; set; }
+
+        public IHttpService HttpService { get; set; }
+
+        public ReaderService(string username, string password, string clientName)
+        { 
+
+        }
+
+        public void Dispose()
+        {
+            if (HttpService != null)
+                HttpService.Dispose();
+        }
+
+    }
+}
