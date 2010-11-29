7c71d26dcbeae2512692ca890025c559b48168cb
simone.chiaretta <simone.chiaretta@gmail.com>
2010-11-29 02:14:54 +0000

    Changed exclusion list to an enum of states rather then strings added "magic" sorting

--- GoogleReaderConnector/trunk/GoogleReaderConsole/Program.cs
+++ GoogleReaderConnector/trunk/GoogleReaderConsole/Program.cs
@@ -25,99 +25,99 @@ namespace CodeClimber.GoogleReaderConsole
 
             ReaderService rdr = new ReaderService(builder, service);
 
-            try
-            {
-                Console.WriteLine(" -----------Authentication ------------------");
-                bool login = loginService.Login();
-                if (!login)
-                    Console.WriteLine("Authentication failed, please check your username and password");
-            }
-            catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
-            catch (AuthTokenException ex) { Console.WriteLine("Error retrieving authorization token"); }
-            catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
+            //try
+            //{
+            //    Console.WriteLine(" -----------Authentication ------------------");
+            //    bool login = loginService.Login();
+            //    if (!login)
+            //        Console.WriteLine("Authentication failed, please check your username and password");
+            //}
+            //catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
+            //catch (AuthTokenException ex) { Console.WriteLine("Error retrieving authorization token"); }
+            //catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
 
-            bool doAgain;
-            do
-            {
-                doAgain = false;
-                try
-                {
-                    Console.WriteLine(" ----------- Post list ------------------");
-                        //foreach (FeedItem item in rdr.GetFeed("http://feeds.feedburner.com/codeclimber", new ReaderParameters() { Direction = ItemDirection.Descending, MaxItems = 20 }))
-                        foreach (FeedItem item in rdr.GetState(StateType.ReadingList, new ReaderFeedParameters() {  Direction = ItemDirection.Default, MaxItems = 10 }))
-                        //foreach (FeedItem item in rdr.GetTag("ALT.net", new ReaderParameters()))
-                        {
-                            Console.WriteLine(item.Blog.Title + " : " + item.Title + " by " + item.Author);
-                        }
+            //bool doAgain;
+            //do
+            //{
+            //    doAgain = false;
+            //    try
+            //    {
+            //        Console.WriteLine(" ----------- Post list ------------------");
+            //            //foreach (FeedItem item in rdr.GetFeed("http://feeds.feedburner.com/codeclimber", new ReaderParameters() { Direction = ItemDirection.Descending, MaxItems = 20 }))
+            //            foreach (FeedItem item in rdr.GetState(StateType.ReadingList, new ReaderFeedParameters() {  Direction = ItemDirection.Default, MaxItems = 10 }))
+            //            //foreach (FeedItem item in rdr.GetTag("ALT.net", new ReaderParameters()))
+            //            {
+            //                Console.WriteLine(item.Blog.Title + " : " + item.Title + " by " + item.Author);
+            //            }
 
-                    Console.WriteLine(" ----------- Friend Detail ------------------");
+            //        Console.WriteLine(" ----------- Friend Detail ------------------");
 
-                    Friend friend = rdr.GetFriend("14290265284323789574");
-                    Console.WriteLine(friend.DisplayName);
+            //        Friend friend = rdr.GetFriend("14290265284323789574");
+            //        Console.WriteLine(friend.DisplayName);
 
-                    Console.WriteLine(" ----------- Friend List ------------------");
+            //        Console.WriteLine(" ----------- Friend List ------------------");
 
-                    foreach (var item in rdr.GetFriends())
-                    {
-                        Console.WriteLine(item.DisplayName);
-                    }
+            //        foreach (var item in rdr.GetFriends())
+            //        {
+            //            Console.WriteLine(item.DisplayName);
+            //        }
 
-                    Console.WriteLine(" ----------- Unread Count ------------------");
+            //        Console.WriteLine(" ----------- Unread Count ------------------");
 
 
-                    var unreadInfo = rdr.GetUnreadCount();
+            //        var unreadInfo = rdr.GetUnreadCount();
 
-                    Console.WriteLine("New Feeds: " + unreadInfo.Single(u => u.Type == CountType.All).Count);
+            //        Console.WriteLine("New Feeds: " + unreadInfo.Single(u => u.Type == CountType.All).Count);
 
-                    var sharedList = unreadInfo.SingleOrDefault(u => u.Type == CountType.AllShared);
-                    if (sharedList != null)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Shared by friends: " + sharedList.Count);
-                        foreach (var info in unreadInfo.Where(u => u.Type == CountType.Shared))
-                        {
-                            Console.WriteLine(" - {0} ({1})", rdr.GetFriend(info.UserId).DisplayName, info.Count);
-                        }
-                    }
+            //        var sharedList = unreadInfo.SingleOrDefault(u => u.Type == CountType.AllShared);
+            //        if (sharedList != null)
+            //        {
+            //            Console.WriteLine();
+            //            Console.WriteLine("Shared by friends: " + sharedList.Count);
+            //            foreach (var info in unreadInfo.Where(u => u.Type == CountType.Shared))
+            //            {
+            //                Console.WriteLine(" - {0} ({1})", rdr.GetFriend(info.UserId).DisplayName, info.Count);
+            //            }
+            //        }
 
-                    Console.WriteLine();
-                    Console.WriteLine("Unread count by State");
-                    foreach (var info in unreadInfo.Where(u => u.Type == CountType.State).OrderBy(u => u.Count))
-                    {
-                        Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("Unread count by Label");
-                    foreach (var info in unreadInfo.Where(u => u.Type == CountType.Label).OrderByDescending(u => u.Count))
-                    {
-                        Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("Unread count by Feed");
-                    foreach (var info in unreadInfo.Where(u => u.Type == CountType.Feed).OrderByDescending(u => u.Count))
-                    {
-                        Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
-                    }
-                }
-                catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
-                catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
-                catch (LoginFailedException)
-                {
-                    try
-                    {
-                        Console.WriteLine(" -----------Authentication ------------------");
-                        bool login = loginService.Login();
-                        if (!login)
-                            Console.WriteLine("Authentication failed, please check your username and password");
-                        else
-                            doAgain = true;
-                    }
-                    catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
-                    catch (AuthTokenException ex) { Console.WriteLine("Error retrieving authorization token"); }
-                    catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
-                }
+            //        Console.WriteLine();
+            //        Console.WriteLine("Unread count by State");
+            //        foreach (var info in unreadInfo.Where(u => u.Type == CountType.State).OrderBy(u => u.Count))
+            //        {
+            //            Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
+            //        }
+            //        Console.WriteLine();
+            //        Console.WriteLine("Unread count by Label");
+            //        foreach (var info in unreadInfo.Where(u => u.Type == CountType.Label).OrderByDescending(u => u.Count))
+            //        {
+            //            Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
+            //        }
+            //        Console.WriteLine();
+            //        Console.WriteLine("Unread count by Feed");
+            //        foreach (var info in unreadInfo.Where(u => u.Type == CountType.Feed).OrderByDescending(u => u.Count))
+            //        {
+            //            Console.WriteLine(" - {0} ({1})", info.Name, info.Count);
+            //        }
+            //    }
+            //    catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
+            //    catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
+            //    catch (LoginFailedException)
+            //    {
+            //        try
+            //        {
+            //            Console.WriteLine(" -----------Authentication ------------------");
+            //            bool login = loginService.Login();
+            //            if (!login)
+            //                Console.WriteLine("Authentication failed, please check your username and password");
+            //            else
+            //                doAgain = true;
+            //        }
+            //        catch (NetworkConnectionException ex) { Console.WriteLine(ex.Message); }
+            //        catch (AuthTokenException ex) { Console.WriteLine("Error retrieving authorization token"); }
+            //        catch (GoogleResponseException ex) { Console.WriteLine(String.Format("There was a problem with the connection: {0}, {1}", ex.StatusCode, ex.Message)); }
+            //    }
 
-            } while (doAgain);
+            //} while (doAgain);
 
 
 
@@ -196,7 +196,7 @@ namespace CodeClimber.GoogleReaderConsole
 
         private static void TestGetTag(ReaderServiceAsync rdr)
         {
-            rdr.GetTag("ALT.net", new ReaderFeedParameters { Direction = ItemDirection.Default, MaxItems = 100 },
+            rdr.GetTag("ALT.net", new ReaderFeedParameters { Direction = ItemDirection.Default, MaxItems = 5},
                             items =>
                                 {
                                     foreach (var item in items)
